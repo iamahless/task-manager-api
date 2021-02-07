11e00d2d5385e4cb7e0e67db97644371a8eae547
Alexander Garuba <alexandergaruba96@gmail.com>
2021-02-07 10:29:54 +0100

    packages updated

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
+require('@knuckleswtf/scribe')(app)
+
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-module.exports = app;
